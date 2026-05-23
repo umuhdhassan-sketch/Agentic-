2325518c4b41ffdeb51f7f5944ea4bc453271767
umuhdhassan-sketch <umuhdhassan@gmail.com>
2026-05-23 08:18:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,5 @@
 import logging
 import os
-from http.server import BaseHTTPRequestHandler, HTTPServer
-import threading
 from groq import Groq
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
@@ -9,24 +7,10 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 # Sanya lambobin sirri
 GROQ_API_KEY = "Gsk_P4Gp6Vcfjcc9NcGXYDSDWGdyb3FYXi5lbYB3W65Xo4MREQD2bOue"
 TELEGRAM_BOT_TOKEN = "8990797862:AAHey5yxI-YWJtMjOvimfJc7GFSsRTkC57c"
-
+RENDER_URL = "https://agentic-markets.onrender.com"
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 client = Groq(api_key=GROQ_API_KEY)
-
-# Tsarin Web Server na karya don gamsar da Render Free Web Service
-class HealthCheckServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
-        self.wfile.write(b"Bot is Live!")
-
-def run_health_server():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), HealthCheckServer)
-    logging.info(f"Health check server running on port {port}")
-    server.serve_forever()
 
 def ask_groq(user_text):
     try:
@@ -52,15 +36,19 @@ async def reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(ai_answer)
 
 def main():
-    # Kunna server din kyauta a bango
-    threading.Thread(target=run_health_server, daemon=True).start()
-    
-    # Kunna bot din Telegram
     app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, reply))
-    print("🚀 Bot is running...")
-    app.run_polling()
+    
+    port = int(os.environ.get("PORT", 10000))
+    
+    print("🚀 Starting Webhook...")
+    app.run_webhook(
+        listen="0.0.0.0",
+        port=port,
+        url_path=TELEGRAM_BOT_TOKEN,
+        webhook_url=f"{RENDER_URL}/{TELEGRAM_BOT_TOKEN}"
+    )
 
 if __name__ == '__main__':
     main()
